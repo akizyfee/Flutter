da313713a57b828d4be403255cfcdc56256a76b4
akizyfee <87452790+akizyfee@users.noreply.github.com>
2023-11-14 23:24:49 +0800

    feat: 學習變量

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:first_app/style_text.dart';
 
+var starAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
+          colors: const [
             Color.fromARGB(137, 158, 35, 35),
             Color.fromARGB(136, 126, 100, 100)
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: starAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(child: StyleText()),
