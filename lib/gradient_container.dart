6a9ead7e3f5ef9bf89213b314fd3165b3f9c9c64
Pause <akidyfee@gmail.com>
2023-11-16 11:58:17 +0800

    feat: 學習使用assets

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -5,21 +5,48 @@ const starAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.colors, {super.key});
+  const GradientContainer(this.colors1, this.colors2, {super.key});
 
-  final Color colors;
+  final Color colors1;
+  final Color colors2;
+
+  void rollDice() {}
 
   @override
   Widget build(context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [colors],
+          colors: [colors1, colors2],
           begin: starAlignment,
           end: endAlignment,
         ),
       ),
-      child: const Center(child: StyleText('hi')),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/dice-2.png',
+              width: 200,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                // padding: const EdgeInsets.only(top: 20),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 28,
+                ),
+              ),
+              child: const Text('Roll Dice'),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,8 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(Color.fromARGB(255, 179, 189, 110)),
+        body: GradientContainer(Color.fromARGB(255, 179, 189, 110),
+            Color.fromARGB(255, 179, 189, 110)),
       ),
     ),
   );
